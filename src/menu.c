1364721499a710dc9ab5bf1141ed6b7f0e620e78
bazzaou <hamza.bazzaou@etu.u-pec.fr>
2023-12-06 16:08:33 +0100

    avance.c

--- src/menu.c
+++ src/menu.c
@@ -1,3 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<graph.h>
+#include"../include/main.h"
+#include"../include/game.h"
 
+void afficher_menu(zone titre, zone selection, zone facile, zone moyen, zone difficile, zone quitter) {
+    ChargerImageFond("./img/backgrounds/menu.gif");
+    dessiner_bouton(titre, "JEU DU SERPENT !", CouleurParNom("black"), CouleurParNom("white"), CouleurParNom("white"), 2);
+    dessiner_bouton(selection, "Selectionnez un niveau :", CouleurParNom("black"), CouleurParNom("white"), CouleurParNom("white"), 1);
+    dessiner_bouton(facile, "Facile", CouleurParNom("black"), CouleurParNom("dark green"), CouleurParNom("dark green"), 1);
+    dessiner_bouton(moyen, "Moyen", CouleurParNom("black"), CouleurParNom("orange"), CouleurParNom("orange"), 1);
+    dessiner_bouton(difficile, "Difficile", CouleurParNom("black"), CouleurParNom("red"), CouleurParNom("red"), 1);
+    dessiner_bouton(quitter, "QUITTER", CouleurParNom("red"), CouleurParNom("red"), CouleurParNom("white"), 1);
+}
+
+void menu(void){
+    zone titre = {490, 60, 260, 55};
+    zone selection = {42, 300, 230, 35};
+    zone facile = {42, 375, 85, 35};
+    zone moyen = {42, 450, 85, 35};
+    zone difficile = {42, 525, 85, 35};
+    zone quitter = {42, 605, 100, 35};
+    afficher_menu(titre, selection, facile, moyen, difficile, quitter);
+    int boucle = 1;
+    while (boucle){
+        SourisPosition();
+        if (SourisCliquee()) {
+            if (check_zone(facile, _X, _Y)){
+                if(game(2, 2)) {
+                    afficher_menu(titre, selection, facile, moyen, difficile, quitter);
+                } else {
+                    boucle = 0;
+                }
+            }
+            if (check_zone(moyen, _X, _Y)) {
+                if (game(4, 4)) {
+                    afficher_menu(titre, selection, facile, moyen, difficile, quitter);
+                } else {
+                    boucle = 0;
+                }
+            }
+            if (check_zone(difficile, _X, _Y)) {
+                if (game(8, 8)) {
+                    afficher_menu(titre, selection, facile, moyen, difficile, quitter);
+                } else {
+                    boucle = 0;
+                }
+            }
+            if (check_zone(quitter, _X, _Y)) boucle = 0;
+        }
+    }
+}
